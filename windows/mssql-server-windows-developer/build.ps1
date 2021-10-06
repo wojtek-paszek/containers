f1b6a97debccd4774689c02eb0348649047406b4
wojciech.paszek <wojciech.paszek@gmx.com>
2021-10-06 17:29:01 +0200

    pass VERSION in build time

--- windows/mssql-server-windows-developer/build.ps1
+++ windows/mssql-server-windows-developer/build.ps1
@@ -1,1 +1,6 @@
-docker build . -t pachoo/mssql-server-windows-developer:1809
+param(
+    [Parameter(Mandatory=$false)]
+    [string]$VERSION
+)
+
+docker build -t pachoo/mssql-server-windows-developer:$VERSION --build-arg VERSION=$VERSION .
